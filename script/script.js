ef07ccea5c4bee02f70a17c2bb4c8b35ef8ce9ac
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-08 20:26:44 +0100

    code revision

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
-$(function () {
-  /* --- Dropdown menu --- */
-  $(".dropdown > a").click(function () {
-    $(this).parent().siblings().find("ul").fadeOut(200);
-    $(this).next("ul").stop(true, false, true).fadeToggle(200);
-    return false;
+/* --- Dropdown menu --- */
+$(".dropdown > a").click(function () {
+  $(this).parent().siblings().find("ul").fadeOut(200);
+  $(this).next("ul").stop(true, false, true).fadeToggle(200);
+  return false;
+});
+
+/* --- Image slider --- */
+$(document).ready(function ($) {
+  $("#slider").QCslider({
+    duration: 5000
   });
-  /* --- Image slider --- */
-  $(document).ready(function ($) {
-    $("#slider").QCslider({
-      duration: 5000
-    });
-  });
-  /* --- Read more dropdown --- */
-  $("h3").on("click", function () {
-    $(this).siblings("p").slideToggle(500);
-    let opacityValue = $(this).hasClass("active") ? 1 : 1;
-    $(this).parent().siblings().fadeTo(500, opacityValue);
-    $(this).toggleClass("active");
-  });
+});
+
+/* --- Read more dropdown --- */
+$("h3").on("click", function () {
+  $(this).siblings("p").slideToggle(500);
+  let opacityValue = $(this).hasClass("active") ? 1 : 1;
+  $(this).parent().siblings().fadeTo(500, opacityValue);
+  $(this).toggleClass("active");
 });
